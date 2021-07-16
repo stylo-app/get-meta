e9420d2722c592406a6eb04232d8df446a3453a5
Thibaut Sardan <github@thib.top>
2021-07-16 22:56:23 +0200

    bump

--- config.ts
+++ config.ts
@@ -21,11 +21,6 @@ export default [
     rpcEndpoint: 'wss://fullnode.centrifuge.io'
   },
   {
-    genesis: '0x742a2ca70c2fda6cee4f8df98d64c4c670a052d9568058982dad9d5a7a135c5b',
-    name: 'edgeware',
-    rpcEndpoint: 'ws://mainnet1.edgewa.re:9944'
-  },
-  {
     genesis: '0xe7c3d5edde7db964317cd9b51a3a059d7cd99f81bdbce14990047354334c9779',
     name: 'rococo',
     rpcEndpoint: 'wss://rococo-rpc.polkadot.io'
@@ -41,8 +36,18 @@ export default [
     rpcEndpoint: 'wss://kusama-statemine-rpc.paritytech.net'
   },
   {
-    genesis: '0xf7a99d3cb92853d00d5275c971c132c074636256583fee53b3bbe60d7b8769ba',
-    name: 'kulupu',
-    rpcEndpoint: 'wss://rpc.kulupu.corepaper.org/ws'
+    genesis: '0xbaf5aabe40646d11f0ee8abbdc64f4a4b7674925cba08e4a05ff9ebed6e2126b',
+    name: 'karura',
+    rpcEndpoint: 'wss://karura-rpc-1.aca-api.network'
+  },
+  {
+    genesis: '0x401a1f9dca3da46f5c4091016c8a2f26dcea05865116b286f60f668207d1474b',
+    name: 'moonriver',
+    rpcEndpoint: 'wss://wss.moonriver.moonbeam.network'
+  },
+  {
+    genesis: '0xf1cf9022c7ebb34b162d5b5e34e705a5a740b2d0ecc1009fb89023e62a488108',
+    name: 'shidden',
+    rpcEndpoint: 'wss://rpc.shiden.plasmnet.io'
   }
 ] as Config[];
